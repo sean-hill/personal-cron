23dece36706ff81b64886ad33a0055ec22f9f933
Sean Hill <sean.daryl.hill@gmail.com>
2015-06-06 21:10:58 -0700

    Don't do gospel task either

--- cron.js
+++ cron.js
@@ -8,7 +8,7 @@ var Selenium = require(path.resolve(__dirname, './util/selenium'));
 
 // Start jobs
 require(jobPath + '/forecast');
-require(jobPath + '/daily-gospel');
+// require(jobPath + '/daily-gospel');
 
 // Start Selenium
 // Selenium.go();
